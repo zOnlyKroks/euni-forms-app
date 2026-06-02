ba948c3578f3c7cca745d2e1c95469c73a46da96
zOnlyKroks <finnrades@gmail.com>
2026-06-02 12:47:51 +0200

    Fix invalid yaml

--- apps/aa-euni-forms/euniforms/tests/test_forms.py
+++ apps/aa-euni-forms/euniforms/tests/test_forms.py
@@ -2,6 +2,7 @@
 
 from django.test import TestCase
 from django.contrib.auth.models import User, Group
+from django import forms
 from unittest.mock import Mock, MagicMock
 from decimal import Decimal
 import datetime
